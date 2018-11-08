123ceea6eccf8683749bf72e85dd9fecbefebb5d
rvanboxtel <romek.boxtel@gmail.com>
2018-11-08 11:03:00 +0100

    Update roues
    It should work but it doesn't somehow

--- src/app/routes.php
+++ src/app/routes.php
@@ -2,30 +2,30 @@
 declare(strict_types=1);
 
 // Forums
-$router->get('', 'ForumController@index');
-$router->get('builds', 'ForumController@show');
-$router->post('builds/comment', 'ForumController@comment');
-$router->get('create', 'ForumController@create');
-$router->post('create/post', 'ForumController@post');
+$router->get('~s1127680/P1_OOAPP_Tentamen', 'ForumController@index');
+$router->get('~s1127680/P1_OOAPP_Tentamen/builds', 'ForumController@show');
+$router->post('~s1127680/P1_OOAPP_Tentamen/builds/comment', 'ForumController@comment');
+$router->get('~s1127680/P1_OOAPP_Tentamen/create', 'ForumController@create');
+$router->post('~s1127680/P1_OOAPP_Tentamen/create/post', 'ForumController@post');
 
 //Login/logout routes
-$router->get('login', 'LoginController@create');
-$router->post('login', 'LoginController@store');
-$router->get('banned', 'LoginController@banned');
-$router->get('logout', 'LoginController@destroy');
+$router->get('~s1127680/P1_OOAPP_Tentamen/login', 'LoginController@create');
+$router->post('~s1127680/P1_OOAPP_Tentamen/login', 'LoginController@store');
+$router->get('~s1127680/P1_OOAPP_Tentamen/banned', 'LoginController@banned');
+$router->get('~s1127680/P1_OOAPP_Tentamen/logout', 'LoginController@destroy');
 
 //Register routes
-$router->get('register', 'RegistrationController@create');
-$router->post('register', 'RegistrationController@store');
+$router->get('~s1127680/P1_OOAPP_Tentamen/register', 'RegistrationController@create');
+$router->post('~s1127680/P1_OOAPP_Tentamen/register', 'RegistrationController@store');
 
 //User routes
-$router->get('users', 'UsersController@index');
-$router->get('edit', 'UsersController@edit');
-$router->post('users', 'UsersController@store');
-$router->post('users/ban', 'UsersController@ban');
-$router->post('users/update', 'UsersController@update');
-$router->post('users/destroy', 'UsersController@destroy');
+$router->get('~s1127680/P1_OOAPP_Tentamen/users', 'UsersController@index');
+$router->get('~s1127680/P1_OOAPP_Tentamen/edit', 'UsersController@edit');
+$router->post('~s1127680/P1_OOAPP_Tentamen/users', 'UsersController@store');
+$router->post('~s1127680/P1_OOAPP_Tentamen/users/ban', 'UsersController@ban');
+$router->post('~s1127680/P1_OOAPP_Tentamen/users/update', 'UsersController@update');
+$router->post('~s1127680/P1_OOAPP_Tentamen/users/destroy', 'UsersController@destroy');
 
 
 //Moderation Panels
-$router->get('moderation', 'ModerationController@index');
+$router->get('~s1127680/P1_OOAPP_Tentamen/moderation', 'ModerationController@index');
